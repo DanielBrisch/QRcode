35d4076d7fc0d0ff9d86a5d00bcbc1dbe6d7435d
DanielBrisch <danielbrisch157@gmail.com>
2024-02-21 00:20:26 -0300

    row pronta

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,44 +12,88 @@ class _HomePage extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-
     return Scaffold(
       body: SingleChildScrollView(
-        child: Container(
-          width: size.width,
-          height: size.height,
-          decoration: const BoxDecoration(color: Colors.white),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding:
-                    EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-                child: Row(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(50),
-                      child: Container(
-                        height: 70, // Altura igual ao raio do borderRadius
-                        width: 70, // Largura igual ao raio do borderRadius
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(50),
-                          border: Border.all(color: Colors.white, width: 2),
-                        ),
-                        child: Image.asset('lib/assets/img/happyguy.png',
-                            fit: BoxFit.cover),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              // Expanded(
-              //   //child: YourContentWidget(), // Adicione o conteúdo da sua tela aqui
-              // ),
+          child: Container(
+        width: size.width,
+        height: size.height,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromRGBO(220, 226, 232, 1.0),
+              Colors.white,
             ],
           ),
         ),
-      ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(25, 70, 25, 0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const CircleAvatar(
+                    radius: 35,
+                    backgroundImage: AssetImage('lib/assets/img/happyguy.png'),
+                  ),
+                  const SizedBox(width: 10),
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Daniel',
+                        style: TextStyle(
+                            fontSize: 17, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Cibolli',
+                        style: TextStyle(
+                            fontSize: 17, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        'Mobile developer',
+                        style: TextStyle(fontSize: 14),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(width: 52),
+                  Container(
+                    height: 40,
+                    width: 100,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.horizontal(
+                          left: Radius.circular(8), right: Radius.circular(8)),
+                    ),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.info),
+                            onPressed: () {},
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.settings),
+                            onPressed: () {},
+                          ),
+                        ]),
+                  ),
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.fromLTRB(25, 110, 25, 0),
+              child: Text(
+                'Welcome\nto QRcode',
+                style: TextStyle(fontSize: 40),
+              ),
+            )
+          ],
+        ),
+      )),
     );
   }
 }
